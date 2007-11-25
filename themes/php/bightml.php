f0282a59ce52359d54da701d23c1435e9344b7c5
Hannes Magnusson <bjori@php.net>
2007-11-25 14:08:49 +0000

    Add some html headers and set the charset to UTF-8

--- themes/php/bightml.php
+++ themes/php/bightml.php
@@ -19,7 +19,14 @@ class bightml extends phpdotnet implements PhDTheme {
         fclose($this->stream);
     }
     public function header() {
-        fwrite($this->stream, "<html><body>");
+        fwrite($this->stream, '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+ <head>
+  <title>PHP Manual</title>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+ </head>
+ <body>');
+
     }
     public function footer() {
         fwrite($this->stream, "</body></html>");
