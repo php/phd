12623c856a6aedad573e745a75ce62cb30e4edd5
Rudy Nappée <loudi@php.net>
2008-05-29 14:34:17 +0000

    * Added the CHM rendering as a PhDTheme * Modified config.php to support the CHM theme * Added a howto for generating CHM file * bug : The flag -t to render just one theme didn't work

--- phpdotnet/phd/Config.php
+++ phpdotnet/phd/Config.php
@@ -17,6 +17,7 @@ class PhDConfig
                     'phpweb',
                     'chunkedhtml',
                     'bightml',
+                    'chmsource',
                 ),
             ),
         ),
