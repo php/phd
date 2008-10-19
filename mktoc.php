0897dde2d05b2676642150e883959666a6f030eb
Hannes Magnusson <bjori@php.net>
2008-10-19 14:07:13 +0000

    MFH: Comments, CS & WS

--- mktoc.php
+++ mktoc.php
@@ -1,5 +1,37 @@
 <?php
-/*  $Id$ */
+/**
+* Creates the internal table of contents array.
+* It is used to look up referenced ids and referenced names.
+*
+* The TOC is stored in the $ID variable. It is not saved, just generated.
+* Generation works by using PhDReader to iterate over each tag, stopping at
+* title (or similar) tags and chunk elements.
+* Chunk element names are defined in PhDReader::OPEN_CHUNK and
+* PhDReader::CLOSE_CHUNK.
+* Title tags are title, titleabbrev, refname and refpurpose.
+*
+* IDs are taken from the xml:id="" attributes of the tags.
+*
+* It has the following format:
+* array(
+*     '$id' => array(
+*         'sdesc'    => 'Short description (e.g. title)',
+*         'ldesc'    => 'Long description (e.g. titleabbrev)',
+*         'children' => array('array', 'of', 'children', 'ids'),
+*         'parent'   => 'parent_id',
+*         'filename' => 'filename for chunk'
+*     ),
+*     [..more..]
+* )
+*
+* This script also creates the associative $REFS array, using the normalized
+* <refname> value as key and its xml:id as value. This is used in
+* PhDHelper::getRefnameLink(), and allows easy refname->link resolution.
+*
+* @package PhD
+* @version CVS: $Id$
+*/
+
 $r = new PhDReader($OPTIONS);
 $VARS = $CLASSES = $REFS = $FILENAMES = array();
 $CURRENT_FILENAME = $LAST_CHUNK = "";
@@ -8,7 +40,7 @@ $CURRENT_FILENAME = $LAST_CHUNK = "";
 $PARENTS = array(-1 => "ROOT", 1 => "manual", 2 => "manual");
 $lastid = 0;
 
-while($r->read()) {
+while ($r->read()) {
     if (!($id = $r->getID())) {
         $name = $r->name;
         if (empty($IDs[$lastid]["sdesc"])) {
@@ -19,7 +51,7 @@ while($r->read()) {
                 $VARS[$refname] = $lastid;
                 continue;
             }
-            elseif($name == "titleabbrev") {
+            else if($name == "titleabbrev") {
                 $IDs[$lastid]["sdesc"] = $class = trim($r->readContent($name));
                 $elm = $r->getParentTagName();
                 if ($elm == "phpdoc:classref" || $elm == "phpdoc:exceptionref") {
@@ -27,7 +59,7 @@ while($r->read()) {
                 }
                 continue;
             }
-        } elseif($name == "refname") {
+        } else if ($name == "refname") {
             $refname = trim($r->readContent($name));
             $ref = strtolower(str_replace(array("_", "::", "->"), array("-", "-", "-"), $refname));
             $REFS[$ref] = $lastid;
@@ -41,7 +73,8 @@ while($r->read()) {
 
         continue;
     }
-    switch($r->isChunk) {
+
+    switch ($r->isChunk) {
     case PhDReader::OPEN_CHUNK:
         $CURRENT_FILENAME = $FILENAMES[] = $PARENTS[$r->depth] = $id;
         break;
@@ -62,7 +95,12 @@ while($r->read()) {
 
     $IDs[$id] = array(
         "filename" => $CURRENT_FILENAME,
-        "parent"   => $r->isChunk ? (isset($PARENTS[$r->depth-1]) ? $PARENTS[$r->depth-1] : $PARENTS[$r->depth-2]) : $CURRENT_FILENAME,
+        "parent"   => $r->isChunk
+                        ? (isset($PARENTS[$r->depth-1])
+                            ? $PARENTS[$r->depth-1]
+                            : $PARENTS[$r->depth-2]
+                        )
+                        : $CURRENT_FILENAME,
         "sdesc"    => null,
         "ldesc"    => null,
         "children" => array(),
@@ -75,4 +113,3 @@ while($r->read()) {
 * vim600: sw=4 ts=4 fdm=syntax syntax=php et
 * vim<600: sw=4 ts=4
 */
-
