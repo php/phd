ef2a7d8a5c834ec3d6011fe50f728e2a7427ac54
Christian Weiske <cweiske@php.net>
2008-10-30 05:16:45 +0000

    Implement request #46411: Allow random chunking depths We can use phd:chunk="true" in any element with an ID now to get a chunk of it. Preferably <section> and other elements that are prepared to be chunked :)

--- phpdotnet/phd/Reader/Legacy.php
+++ phpdotnet/phd/Reader/Legacy.php
@@ -109,6 +109,21 @@ class PhDReader extends XMLReader {
         return "";
     } /* }}} */
 
+    /**
+     * Check if the element wants to be chunked.
+     * Reads the element's 'phd:chunk="true"' attribute
+     *
+     * @return boolean true if the element shall be chunked
+     */
+    public function getWantsToBeChunked() { /* {{{ */
+        if ($this->hasAttributes && XMLReader::moveToAttributeNs("chunk", self::XMLNS_PHD)) {
+            $isChunk = $this->value == 'true';
+            XMLReader::moveToElement();
+            return $isChunk;
+        }
+        return false;
+    } /* }}} */
+
     public function getParentTagName() { /* {{{ */
         return $this->STACK[$this->depth-1];
     } /* }}} */
@@ -205,6 +220,8 @@ class PhDReader extends XMLReader {
     public function isChunk($tag, $id) { /* {{{ */
         if (!$id) {
             return false;
+        } else if ($this->getWantsToBeChunked()) {
+            return true;
         } else if (isset($this->CHUNK_ME[$tag])) {
             $isChunk = $this->CHUNK_ME[$tag];
             if (is_array($isChunk)) {
