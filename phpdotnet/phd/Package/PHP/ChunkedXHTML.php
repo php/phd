989c2fcb011dc461e1eaff3d2a147b5988e68a63
Moacir de Oliveira Miranda Júnior <moacir@php.net>
2009-07-05 06:36:46 +0000

    added chunked xhtml format

---
+++ phpdotnet/phd/Package/PHP/ChunkedXHTML.php
@@ -0,0 +1,71 @@
+<?php
+namespace phpdotnet\phd;
+
+class Package_PHP_ChunkedXHTML extends Package_PHP_Web {
+    private $nav = "";
+    protected $formatname = "PHP-Chunked-XHTML";
+    protected $title = "PHP Manual";
+
+    public function __construct() {
+        parent::__construct();
+        $this->ext = "html";
+    }
+
+    public function __destruct() {
+        parent::__destruct();
+    }
+
+    public function header($id) {
+        $title = Format::getShortDescription($id);
+        $header = <<<HEADER
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+ <head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>$title</title>
+ </head>
+ <body>
+HEADER;
+        $parent = Format::getParent($id);
+        $next = $prev = $up = array("href" => null, "desc" => null);
+        if ($parent && $parent != "ROOT") {
+            if ($prevId = Format::getPrevious($id)) {
+                $prev = array(
+                    "href" => $this->getFilename($prevId) . "." . $this->ext,
+                    "desc" => $this->getShortDescription($prevId),
+                );
+            }
+            if ($nextId = Format::getNext($id)) {
+                $next = array(
+                    "href" => $this->getFilename($nextId) . "." . $this->ext,
+                    "desc" => $this->getShortDescription($nextId),
+                );
+            }
+            if ($parentId = Format::getParent($id)) {
+                $up = array(
+                    "href" => $this->getFilename($parentId) . "." . $this->ext,
+                    "desc" => $this->getShortDescription($parentId),
+                );
+            }
+        }
+
+        $nav = <<<NAV
+<div class="manualnavbar" style="text-align: center;">
+ <div class="prev" style="text-align: left; float: left;"><a href="{$prev["href"]}">{$prev["desc"]}</a></div>
+ <div class="next" style="text-align: right; float: right;"><a href="{$next["href"]}">{$next["desc"]}</a></div>
+ <div class="up"><a href="{$up["href"]}">{$up["desc"]}</a></div>
+ <div class="home"><a href="index.html">PHP Manual</a></div>
+</div>
+NAV;
+        $header .= $nav . "<hr />";
+        $this->nav = $nav;
+        return $header;
+    }
+
+    public function footer($id) {
+        $nav = $this->nav;
+        $this->nav = "";
+        return "<hr />$nav</body></html>\n";
+    }
+
+}
