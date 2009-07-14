3fafbd847778e4ca9b671b36dd6d5f9a1fd622ac
Moacir de Oliveira Miranda Júnior <moacir@php.net>
2009-07-14 05:43:39 +0000

    formats kdevelop, pdf and bigpdf added

--- phpdotnet/phd/Package/PHP/Factory.php
+++ phpdotnet/phd/Package/PHP/Factory.php
@@ -23,6 +23,17 @@ class Package_PHP_Factory extends Format_Factory {
         return new Package_PHP_Functions();
     }
 
+    public function createKDevelopFormat() {
+        return new Package_PHP_KDevelop();
+    }
+
+    public function createPDFFormat() {
+        return new Package_PHP_PDF();
+    }
+
+    public function createBigPDFFormat() {
+        return new Package_PHP_BigPDF();
+    }
 /*    
     public function createChmFormat() {
         return new Package_PHP_CHM();
