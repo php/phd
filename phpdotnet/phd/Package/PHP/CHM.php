b40f7ae4dfe6b8046871efd1704f606feba606d4
Richard Quadling <rquadling@php.net>
2009-08-18 09:45:17 +0000

    CHM is not "Flat"

--- phpdotnet/phd/Package/PHP/CHM.php
+++ phpdotnet/phd/Package/PHP/CHM.php
@@ -278,7 +278,6 @@ Default topic=res' . DIRECTORY_SEPARATOR . 'index.html
 Default Window=doc
 Display compile progress=Yes
 Enhanced decompilation=Yes
-Flat=Yes
 Full-text search=Yes
 Index file=php_manual_' . $lang . '.hhk
 Language=' . $this->LANGUAGES[$lang]["langcode"] . '
