e34161cbed3c71e7c19ebf7ca8b42c56fec52af8
Richard Quadling <rquadling@php.net>
2009-11-02 10:18:41 +0000

    New verbosity levels : VERBOSE_ERRORS (all PHP and user triggered errors), VERBOSE_INFO (Indexing, rendering, reading and writing informationals) and VERBOSE_WARNINGS (No version, broken links, missing attributes).

--- phpdotnet/phd/functions.php
+++ phpdotnet/phd/functions.php
@@ -5,6 +5,9 @@ namespace phpdotnet\phd;
 /* {{{ PhD error & message handler */
 
 // PhD verbose flags
+define('VERBOSE_ERRORS',                 (E_USER_DEPRECATED            << 1) - 1);
+
+// Informationals
 define('VERBOSE_INDEXING',               E_USER_DEPRECATED             << 1);
 define('VERBOSE_FORMAT_RENDERING',       VERBOSE_INDEXING              << 1);
 define('VERBOSE_THEME_RENDERING',        VERBOSE_FORMAT_RENDERING      << 1);
@@ -13,12 +16,20 @@ define('VERBOSE_PARTIAL_READING',        VERBOSE_RENDER_STYLE          << 1);
 define('VERBOSE_PARTIAL_CHILD_READING',  VERBOSE_PARTIAL_READING       << 1);
 define('VERBOSE_TOC_WRITING',            VERBOSE_PARTIAL_CHILD_READING << 1);
 define('VERBOSE_CHUNK_WRITING',          VERBOSE_TOC_WRITING           << 1);
+
+define('VERBOSE_INFO',                   ((VERBOSE_CHUNK_WRITING       << 1) - 1) & ~VERBOSE_ERRORS);
+
+
+// Warnings
 define('VERBOSE_NOVERSION',              VERBOSE_CHUNK_WRITING         << 1);
 define('VERBOSE_BROKEN_LINKS',           VERBOSE_NOVERSION             << 1);
 define('VERBOSE_MISSING_ATTRIBUTES',     VERBOSE_BROKEN_LINKS          << 1);
 
-define('VERBOSE_ALL',                    (VERBOSE_MISSING_ATTRIBUTES   << 1)-1);
-define('VERBOSE_DEFAULT',                (VERBOSE_ALL^(VERBOSE_PARTIAL_CHILD_READING|VERBOSE_CHUNK_WRITING|VERBOSE_NOVERSION|VERBOSE_BROKEN_LINKS|VERBOSE_MISSING_ATTRIBUTES)));
+define('VERBOSE_WARNINGS',               ((VERBOSE_MISSING_ATTRIBUTES  << 1) - 1) & ~VERBOSE_ERRORS & ~VERBOSE_INFO);
+
+
+define('VERBOSE_ALL',                    (VERBOSE_MISSING_ATTRIBUTES   << 1) - 1);
+define('VERBOSE_DEFAULT',                (VERBOSE_ALL^(VERBOSE_PARTIAL_CHILD_READING|VERBOSE_CHUNK_WRITING|VERBOSE_WARNINGS)));
 
 $olderrrep = error_reporting();
 error_reporting($olderrrep | VERBOSE_DEFAULT);
