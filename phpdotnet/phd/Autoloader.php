adf4a572a98725a3f3f20d8a28c25c2d2909dbdd
Hannes Magnusson <bjori@php.net>
2009-12-08 19:20:58 +0000

    WS & comment

--- phpdotnet/phd/Autoloader.php
+++ phpdotnet/phd/Autoloader.php
@@ -1,11 +1,14 @@
 <?php
 namespace phpdotnet\phd;
+
 class Autoloader
 {
     public static function autoload($name)
     {
+        // Only try autoloading classes we know about (i.e. from our own namespace)
         if (strncmp('phpdotnet\phd\\', $name, 14) === 0) {
             $file = __INSTALLDIR__ . DIRECTORY_SEPARATOR . str_replace(array('\\', '_'), DIRECTORY_SEPARATOR, $name) . '.php';
+
             // Using fopen() because it has use_include_path parameter.
             if (!$fp = @fopen($file, 'r', true)) {
                 v('Cannot find file for %s: %s', $name, $file, E_USER_ERROR);
@@ -13,6 +16,7 @@ class Autoloader
             fclose($fp);
             require $file;
         }
+
         return false;
     }
 }
