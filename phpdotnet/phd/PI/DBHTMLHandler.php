62c36d8b18a8b94c7f580151e13dce86c6c8df79
Moacir de Oliveira Miranda Júnior <moacir@php.net>
2010-01-06 16:20:43 +0000

    Adding support to DBTimestamp Processing Instruction

--- phpdotnet/phd/PI/DBHTMLHandler.php
+++ phpdotnet/phd/PI/DBHTMLHandler.php
@@ -28,6 +28,10 @@ class PI_DBHTMLHandler extends PIHandler {
         "term-separator"                => "",
         "term-width"                    => "",
         "toc"                           => "",
+
+        //Attributes for <?dbtimestamp
+        "format"                        => "",
+        "padding"                       => "",
     );
 
     public function __construct($format) {
@@ -46,6 +50,12 @@ class PI_DBHTMLHandler extends PIHandler {
         if ($data == "stop-chunking") {
             $this->setAttribute("stop-chunking", true);
         }
+        //Parse <?dbtimestamp
+        if ($target == 'dbtimestamp') {
+            $this->parseDBTimestamp();
+            $this->setAttribute("padding", "");
+            $this->setAttribute("format", "");
+        }
     }
 
     public function setAttribute($attr, $value) {
@@ -56,6 +66,49 @@ class PI_DBHTMLHandler extends PIHandler {
 
     public function getAttribute($attr) {
         return isset($this->attrs[$attr]) ? $this->attrs[$attr] : false;
+    }
+
+    /**
+     * Function to parse dbtimestamp processing instructions
+     * Reference: http://www.sagehill.net/docbookxsl/Datetime.html
+     */
+    public function parseDBTimestamp() {
+        // Array to parse formats from dbtimestamp to date()
+        $parseArray = array(
+            "a"         => "D",         // Day abbreviation
+            "A"         => "l",         // Day name
+            "b"         => "M",         // Month abbreviation
+            "c"         => "c",         // ISO date and time
+            "B"         => "F",         // Month name
+            "d"         => "d",         // Day in month
+            "H"         => "H",         // Hour in day
+            "j"         => "z",         // Day in year
+            "m"         => "m",         // Month in year
+            "M"         => "i",         // Minute in hour
+            "S"         => "s",         // Second in minute
+            "U"         => "W",         // Week in year
+            "w"         => "w",         // Day in week
+            "x"         => "Y-m-dP",    // ISO date
+            "X"         => "H:i:sP",    // ISO time
+            "Y"         => "Y",         // Year
+        );
+        if ($this->getAttribute("padding") == "0") {
+            $parseArray["d"] = "j";
+            $parseArray["H"] = "G";
+            $parseArray["m"] = "n";
+        }
+        $format = $this->getAttribute("format");
+        if (!$format) {
+            return $this->format->appendData(date('c'));
+        }
+        $dateFormat = "";
+        $len = strlen($format);
+        for ($i = 0; $i < $len; $i++) {
+            $dateFormat .= isset($parseArray[$format[$i]])
+                           ? $parseArray[$format[$i]]
+                           : $format[$i];
+        }
+        return $this->format->appendData(date($dateFormat));
     }
 
 }
--- phpdotnet/phd/Package/Generic/XHTML.php
+++ phpdotnet/phd/Package/Generic/XHTML.php
@@ -436,6 +436,7 @@ abstract class Package_Generic_XHTML extends Format_Abstract_XHTML {
 
     protected $pihandlers = array(
         'dbhtml'        => 'PI_DBHTMLHandler',
+        'dbtimestamp'   => 'PI_DBHTMLHandler',
     );
 
     protected $stylesheets = array();
