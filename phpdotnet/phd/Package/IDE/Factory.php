88a9493ddd331d93c4fe81aa24e586e185d1f578
Moacir de Oliveira Miranda Júnior <moacir@php.net>
2010-05-01 20:44:30 +0000

    Adding 2 new output formats to the IDE Package + JSON - Functions in the json formats + PHP - Var exported files

--- phpdotnet/phd/Package/IDE/Factory.php
+++ phpdotnet/phd/Package/IDE/Factory.php
@@ -6,6 +6,8 @@ class Package_IDE_Factory extends Format_Factory {
     private $formats = array(
         'functions'         => 'Package_IDE_Functions',
         'funclist'          => 'Package_IDE_Funclist',
+        'json'              => 'Package_IDE_JSON',
+        'php'               => 'Package_IDE_PHP',
     );
 
     public function __construct() {
