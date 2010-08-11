b604faeda332a5e8b63351464554fb4b3c2e81c0
Moacir de Oliveira Miranda Júnior <moacir@php.net>
2010-08-11 04:13:48 +0000

    Preparing the release tag

--- phpdotnet/phd/Config.php
+++ phpdotnet/phd/Config.php
@@ -8,7 +8,7 @@ if (!defined("__INSTALLDIR__")) {
 
 class Config
 {
-    const VERSION = 'phd-from-svn';
+    const VERSION = '1.0.1';
 
     private static $optionArray = array(
         'output_format'     => array(),
