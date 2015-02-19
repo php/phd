6fc8c88f496cfa97cb858f72e106d012e8ded1ce
Adam Harvey <aharvey@php.net>
2015-02-19 15:46:12 -0800

    simplesect elements should use the same formatting as sect[2-5].

--- phpdotnet/phd/Package/Generic/XHTML.php
+++ phpdotnet/phd/Package/Generic/XHTML.php
@@ -249,7 +249,7 @@ abstract class Package_Generic_XHTML extends Format_Abstract_XHTML {
         'shortaffil'            => 'format_suppressed_tags',
         'sidebar'               => 'format_note',
         'simplelist'            => 'format_itemizedlist', /* FIXME: simplelists has few attributes that need to be implemented */
-        'simplesect'            => 'format_div',
+        'simplesect'            => 'div',
         'simpara'               => array(
             /* DEFAULT */          'p',
             'note'              => 'span',
