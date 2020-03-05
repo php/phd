3177809ebd0cac16708321f5f226b11876e1a948
Christoph M. Becker <cmbecker69@gmx.de>
2020-03-05 10:05:56 +0100

    Support namespaced classes in <type> elements
    Currently, automatic linking to the documentation pages of namespaced
    classes is supported in some elements (e.g. in `<classsynopsisinfo>`),
    but not in `<type>` elements, which can be seen in the `CommonMark`
    documentation[1].  We catch up on that.
    
    [1] <https://www.php.net/manual/en/book.cmark.php>

--- phpdotnet/phd/Package/PHP/XHTML.php
+++ phpdotnet/phd/Package/PHP/XHTML.php
@@ -427,7 +427,7 @@ abstract class Package_PHP_XHTML extends Package_Generic_XHTML {
     }
 
     public function format_type_text($type, $tagname) {
-        $t = strtr(strtolower($type), "_", "-");
+        $t = strtr(strtolower($type), ["_" => "-", "\\" => "-"]);
         $href = $fragment = "";
 
         switch($t) {
