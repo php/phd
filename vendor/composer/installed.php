ec6e1e513263b745bb959fa65b3ff15f38967ded
George Peter Banyard <girgias@php.net>
2021-07-30 18:33:45 +0200

    Add vendor folder to VCS

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '12596032ec2ffb3da9d6d254d6cc6c000e9245e1',
+    'name' => 'php/phd',
+  ),
+  'versions' => 
+  array (
+    'php/phd' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12596032ec2ffb3da9d6d254d6cc6c000e9245e1',
+    ),
+  ),
+);
