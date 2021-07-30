ebbf955fe0714c774e1da9940cd3c1d4acc1ac3d
George Peter Banyard <girgias@php.net>
2021-07-30 18:30:40 +0200

    Fix demo script in API package

--- src/Package/IDE/demo.php
+++ src/Package/IDE/demo.php
@@ -4,13 +4,10 @@ namespace phpdotnet\phd\Package\IDE;
 
 use phpdotnet\phd\Config;
 
-define('__INSTALLDIR__', '@php_dir@' == '@'.'php_dir@' ? dirname(dirname(dirname(dirname(__DIR__)))) : '@php_dir@');
+define('__INSTALLDIR__', '@php_dir@' == '@'.'php_dir@' ? dirname(__DIR__, 3) : '@php_dir@');
 define('DS', DIRECTORY_SEPARATOR);
 
-require __INSTALLDIR__ . DS . 'phpdotnet' . DS . 'phd' . DS . 'Autoloader.php';
-require __INSTALLDIR__ . DS . 'phpdotnet' . DS . 'phd' . DS . 'functions.php';
-
-spl_autoload_register(array("phpdotnet\\phd\\Autoloader", "autoload"));
+require __INSTALLDIR__ . DS . 'vendor' . DS . 'autoload.php';
 
 //FIXME Remove this call to Config
 Config::init(array());
