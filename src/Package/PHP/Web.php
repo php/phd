5383027847870500e3d0521a5c2d235acfcce2fd
George Peter Banyard <girgias@php.net>
2021-07-30 18:52:49 +0200

    Import removeDir() function in PHP/Web

--- src/Package/PHP/Web.php
+++ src/Package/PHP/Web.php
@@ -4,6 +4,7 @@ namespace phpdotnet\phd\Package\PHP;
 use phpdotnet\phd\Config;
 use phpdotnet\phd\Format;
 use phpdotnet\phd\Render;
+use function phpdotnet\phd\removeDir;
 use function phpdotnet\phd\v;
 
 class Web extends XHTML {
