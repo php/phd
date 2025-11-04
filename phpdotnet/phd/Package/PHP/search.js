bd9bcaad37e6f0b16a4607f7e5fbbbed0a56b78f
AllenJB <AllenJB@users.noreply.github.com>
2025-11-04 16:56:26 +0000

    Chunked XHTML with JS search (don't use the localStorage cache for local docs)

--- phpdotnet/phd/Package/PHP/search.js
+++ phpdotnet/phd/Package/PHP/search.js
@@ -20,7 +20,7 @@ const initPHPSearch = async (language) => {
         const key = `search2-${language}`;
         const cache = window.localStorage.getItem(key);
 
-        if (!cache) {
+        if ((!cache) || (language === 'local')) {
             return null;
         }
 
@@ -46,23 +46,22 @@ const initPHPSearch = async (language) => {
         if (language === 'local') {
             items = localSearchIndexes;
         } else {
-            let sourceUrl = (language === 'local' ? 'search-combined.json' : `/js/search-index.php?lang=${language}`);
-            const response = await fetch(sourceUrl);
+            const response = await fetch(`/js/search-index.php?lang=${language}`);
             items = await response.json();
-        }
 
-        try {
-            localStorage.setItem(
-                key,
-                JSON.stringify({
-                    data: items,
-                    time: Date.now(),
-                }),
-            );
-        } catch (e) {
-            // Local storage might be full, or other error.
-            // Just continue without caching.
-            console.error("Failed to cache search index", e);
+            try {
+                localStorage.setItem(
+                    key,
+                    JSON.stringify({
+                        data: items,
+                        time: Date.now(),
+                    }),
+                );
+            } catch (e) {
+                // Local storage might be full, or other error.
+                // Just continue without caching.
+                console.error("Failed to cache search index", e);
+            }
         }
 
         return items;
