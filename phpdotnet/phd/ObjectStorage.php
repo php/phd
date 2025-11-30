ef57fc016fdc6e444f9884fd94bd2ba3a9e65754
lacatoire <louis-arnaud.catoire@external.drivalia.com>
2025-11-30 16:36:16 +0100

    remove the old one

--- phpdotnet/phd/ObjectStorage.php
+++ phpdotnet/phd/ObjectStorage.php
@@ -3,23 +3,18 @@ namespace phpdotnet\phd;
 
 class ObjectStorage extends \SplObjectStorage
 {
-	public function attach(object $object, mixed $info = array()): void {
+	public function attach(object $object, mixed $info = []): void {
 		if (!($object instanceof Format)) {
 			throw new \InvalidArgumentException(
                 'Only classess inheriting ' . __NAMESPACE__ . '\\Format supported'
             );
 		}
 		if (empty($info)) {
-			$info = array(
+			$info = [
 				\XMLReader::ELEMENT => $object->getElementMap(),
 				\XMLReader::TEXT    => $object->getTextMap(),
-			);
+            ];
 		}
-		parent::attach($object, $info);
-
-        if (PHP_VERSION_ID < 80500) {
-            parent::attach($object, $info);
-        }
 
         $this->offsetSet($object, $info);
 	}
